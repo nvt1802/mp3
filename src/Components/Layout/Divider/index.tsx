8b0657c937ece5e290a758ce7f7a22232109b8fd
NGUYEN VAN TAI <45477632+nvt1802@users.noreply.github.com>
2021-08-04 22:04:35 +0700

    update

---
+++ src/Components/Layout/Divider/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Divider = () => {
+  return <div className="sidebar-divide"></div>
+}
+
+export default Divider
--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -6,7 +6,17 @@ interface IProps {}
 const Sidebar: React.FC<IProps> = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar-wrapper"></div>
+      <div className="sidebar-wrapper">
+        <nav className="navbar">
+          <div className="navbar-brand">
+            <div>
+              <button className="btn button">
+                <div className="zmp3-logo"></div>
+              </button>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   )
 }
