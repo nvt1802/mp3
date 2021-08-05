91196540c982a0d88fa60e50678932a425c1b5b2
NGUYEN VAN TAI <45477632+nvt1802@users.noreply.github.com>
2021-08-05 23:13:09 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,17 @@ import Player from 'Components/Player'
 import Sidebar from 'Components/Sidebar'
 
 import UI from 'Assets/Images/Background/iu.jpg'
-import Jennie from 'Assets/Images/Background/jennie.jpg'
-import Jisoo from 'Assets/Images/Background/jisoo.jpg'
-import Lisa from 'Assets/Images/Background/lisa.jpg'
-import Rose from 'Assets/Images/Background/rose.jpg'
+// import Jennie from 'Assets/Images/Background/jennie.jpg'
+// import Jisoo from 'Assets/Images/Background/jisoo.jpg'
+// import Lisa from 'Assets/Images/Background/lisa.jpg'
+// import Rose from 'Assets/Images/Background/rose.jpg'
 
-const listBg = [UI, Jennie, Jisoo, Lisa, Rose]
+// const listBg = [UI, Jennie, Jisoo, Lisa, Rose]
 
 const App = () => {
-  const getBackground = () => {
-    return listBg[Math.floor(Math.random() * 5)]
-  }
-
   return (
     <Fragment>
-      <div
-        className="app-wrapper"
-        style={{ backgroundImage: `url(${getBackground()})` }}
-      >
+      <div className="app-wrapper" style={{ backgroundImage: `url(${UI})` }}>
         <Sidebar />
         <Player />
       </div>
